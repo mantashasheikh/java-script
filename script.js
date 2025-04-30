421336a975e7768872ebb761bede2930073c122f
mantashasheikh <mantashasheikh95844@gmail.com>
2025-04-30 13:27:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+alert("hello world");
+console.log("hiii")
+console.log("my name is mantasha sheikh")
+// this is single line comment
+
+/*this is
+ multiline
+  comment*/
+
+console.error("this is an error")
+console.warn("this is a warning")
