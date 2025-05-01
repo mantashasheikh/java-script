a645341e21df8e5f0e669185d426afee9ac0f40a
mantashasheikh <mantashasheikh95844@gmail.com>
2025-05-01 13:30:27 +0530

    1 april 2025

---
+++ 1 april 2025/script.js
@@ -0,0 +1,46 @@
+var name = "Mantasha sheikh";
+var age = 20;
+console.log(name);
+console.log(age);
+
+var a;
+a=100;
+console.log(a);
+
+let b;
+b=200;
+console.log(b);
+
+var c=200;
+var c=10;
+console.log(c)
+
+
+// premitive data types
+
+
+var x;
+console.log(x, typeof x);
+
+var firstName="harry";
+console.log(firstName, typeof firstName);
+
+var value = null;
+console.log(value, typeof null);
+// null datatype - object (mistake)
+
+
+var something = false;
+console.log(something, typeof something);
+
+var number = 1234567890n;
+console.log(number, typeof number);
+
+var num = 1234;
+console.log(num, typeof num)
+
+var id = Symbol(id);
+console.log(id, typeof id);
+
+
+// reference data types
