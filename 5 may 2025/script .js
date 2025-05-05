be2e38c84d521d4450d0851a9f0fc5f96a75f7f1
mantashasheikh <mantashasheikh95844@gmail.com>
2025-05-05 13:05:10 +0530

    math methods

---
+++ 5 may 2025/script .js
@@ -0,0 +1,60 @@
+let x = Math.PI;
+console.log(x)
+
+// giving the positive value
+x = Math.abs(-78)
+console.log(x)
+
+// returns -1 if number is negative , returns 1 if number is positive
+x= Math.sign(6);
+console.log(x)
+
+// return the square root
+x = Math.sqrt(49);
+console.log(x)
+
+// return the round off value
+x = Math.round(4.123);
+console.log(x)
+
+x = Math.round(4.567);
+console.log(x)
+
+// floor- method rounds the lower value
+x = Math.floor(4.567)
+console.log(x)
+
+// ceil- method round the upper value
+x = Math.ceil(4.1)
+console.log(x)
+
+// trunc -retirns the integer value
+x = Math.trunc(2345.56788)
+console.log(x)
+
+// min- returns the minimum value
+x = Math.min(45 , 32 , 0 , 234 , -234 , -456, -123)
+console.log(x)
+
+// max - returns the maximum value
+x = Math.max(45 , 32 , 0 , 234 , -234 , -456, -123)
+console.log(x)
+
+// returns x to the power of y (x,y)
+x = Math.pow(2,3);
+console.log(x)
+
+// returns random value between 0 and 1
+x = Math.random();
+console.log(x)
+
+// returns number from 0-100
+x = Math.trunc(Math.random()*11);
+console.log(x)
+
+x = Math.floor(Math.random()*10)+1;
+console.log(x)
+
+
+
+
