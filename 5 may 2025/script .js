162d7a7311c82d432a0098aa0b2519f7d66f018f
mantashasheikh <mantashasheikh95844@gmail.com>
2025-05-05 13:31:20 +0530

    date method

--- 5 may 2025/script .js
+++ 5 may 2025/script .js
@@ -1,3 +1,5 @@
+// Math methods :-
+
 let x = Math.PI;
 console.log(x)
 
@@ -54,6 +56,66 @@ console.log(x)
 
 x = Math.floor(Math.random()*10)+1;
 console.log(x)
+
+
+// Question - 
+
+let a = Math.trunc(Math.random()*11);
+console.log("a=",a)
+let b = Math.trunc(Math.random()*6);
+console.log("b=",b)
+let sum = a + b;
+console.log("sum=",sum);
+let difference = a-b;
+console.log("difference=",difference);
+let mul = a*b;
+console.log("mul = ", mul);
+let div = a/b;
+console.log("div = ", div);
+let mod = a%b;
+console.log("mod=",mod);
+
+
+
+// Date methods
+
+let d = new Date();
+console.log(d);
+
+// By default javascript will use the browser's time zone and display a date as a full text string
+
+// there are 4 ways ti create a new date object
+// new Date()
+// new Date(year, month, day, hours, minutes, second, milliseconds)
+// new Date(milliseconds)
+// new Date(date string)
+
+// month 0 - 11
+d = new Date(2020, 4, 3, 23, 34,45, 896);
+console.log(d);
+
+d = new Date(2020, 4, 3, 23, 34,45 );
+console.log(d);
+
+d = new Date(2020, 4, 3, 23, 34);
+console.log(d);
+
+d = new Date(2020, 4, 3, 23 );
+console.log(d);
+
+d = new Date(2020, 4, 3 );
+console.log(d);
+
+d = new Date(2020, 4);
+console.log(d);
+
+
+// milliseconds
+d = new Date(2020);
+console.log(d);
+
+
+
 
 
 
