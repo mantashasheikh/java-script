061d92195ffd41d467b330d8fc0ba77b7a9b4c12
mantashasheikh <mantashasheikh95844@gmail.com>
2025-08-21 22:48:26 +0530

    mini calender

---
+++ 1 may 2025/miniCalender.js
@@ -0,0 +1,17 @@
+
+let week = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+let months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
+let time = new Date() 
+let date = time.getDate()
+let month = time.getMonth()
+let year = time.getFullYear()
+let day = time.getDay()
+
+
+
+
+
+
+
+    
+    
