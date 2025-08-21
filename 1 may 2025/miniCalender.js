1a1d30ea541a33647ef37b721f6733e6751b22f3
mantashasheikh <mantashasheikh95844@gmail.com>
2025-08-21 22:56:48 +0530

    mini calender

--- 1 may 2025/miniCalender.js
+++ 1 may 2025/miniCalender.js
@@ -3,9 +3,16 @@ let week = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturda
 let months = ["January","February","March","April","May","June","July","August","September","October","November","December"]
 let time = new Date() 
 let date = time.getDate()
-let month = time.getMonth()
+document.querySelector("#date").innerHTML = date
+
+let month = months[time.getMonth()]
+document.querySelector("#month").innerHTML = month
+
 let year = time.getFullYear()
-let day = time.getDay()
+document.querySelector("#year").innerHTML = year
+
+let day = week[time.getDay()]
+document.querySelector("#day").innerHTML = day
 
 
 
