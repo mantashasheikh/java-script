0e302cd104fbd738a2f8f7577625fad753e33a7b
mantashasheikh <mantashasheikh95844@gmail.com>
2025-08-21 12:10:11 +0530

    date object

---
+++ 1 may 2025/dateObject.js
@@ -0,0 +1,17 @@
+console.log(new Date())
+console.log(Date())
+
+let time = new Date()
+console.log(time.toDateString())
+console.log(time.getFullYear())
+console.log(time.getMonth())
+console.log(time.getDay())
+console.log(time.getHours())
+console.log(time.getMinutes())
+console.log(time.getSeconds())
+console.log(time.getMilliseconds())
+
+
+
+
+
