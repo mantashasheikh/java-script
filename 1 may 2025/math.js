a66cbe57e7bcb3db90090ff2cea05b2b5096d974
mantashasheikh <mantashasheikh95844@gmail.com>
2025-08-22 12:30:47 +0530

    math object

---
+++ 1 may 2025/math.js
@@ -0,0 +1,12 @@
+console.log(Math.PI)
+console.log(Math.floor(Math.random()*10))
+console.log(Math.floor(Math.random()*100))
+
+// OTP of 4 digit
+console.log(Math.floor(Math.random()*9000)+1000)
+// OTP of 6 digit
+console.log(Math.floor(Math.random()*900000)+100000)
+
+console.log(Math.floor(Math.random()*6)+1)
+
+
