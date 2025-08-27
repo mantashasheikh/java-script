070e58bd3051b16c548d95c6ac6aef12a48870d8
mantashasheikh <mantashasheikh95844@gmail.com>
2025-08-27 12:27:31 +0530

    roll the dice

--- 1 may 2025/dice.js
+++ 1 may 2025/dice.js
@@ -1,17 +1,21 @@
 
-let clickme = ()=>{
+let clickme=()=>{
     let dice =  Math.floor(Math.random()*6)+1
 
+    console.log(dice);
+    
 
-    let vs1 = document.querySelector(`#vs1${dice}`)
-    let vs2 = document.querySelector(`#vs2${dice}`)
-    let vs3 = document.querySelector(`#vs3${dice}`)
-    let vs4 = document.querySelector(`#vs4${dice}`)
-    let vs5 = document.querySelector(`#vs5${dice}`)
-    let vs6 = document.querySelector(`#vs6${dice}`)
+
+    let vs1 = document.querySelector("#vs1")
+    let vs2 = document.querySelector("#vs2")
+    let vs3 = document.querySelector("#vs3")
+    let vs4 = document.querySelector("#vs4")
+    let vs5 = document.querySelector("#vs5")
+    let vs6 = document.querySelector("#vs6")
 
 
     if(dice=="1"){
+        vs1.style.display="block"
         vs2.style.display = "none"
         vs3.style.display = "none"
         vs4.style.display = "none"
@@ -20,21 +24,37 @@ let clickme = ()=>{
     
     }
     else if(dice=="2"){
+        vs1.style.display="block"
+        vs2.style.display="block"
         vs3.style.display = "none"
         vs4.style.display = "none"
         vs5.style.display = "none"
         vs6.style.display = "none"
     }
     else if(dice=="3"){
+        vs1.style.display="block"
+        vs2.style.display="block"
+        vs3.style.display="block"
+
         vs4.style.display = "none"
         vs5.style.display = "none"
         vs6.style.display = "none"
     }
     else if(dice=="4"){
+        vs1.style.display="block"
+        vs2.style.display="block"
+        vs3.style.display="block"
+        vs4.style.display="block"
         vs5.style.display = "none"
         vs6.style.display = "none"
     }
     else if(dice=="5"){
+        vs1.style.display="block"
+        vs2.style.display="block"
+        vs3.style.display="block"
+        vs4.style.display="block"
+        vs5.style.display="block"
+
         vs6.style.display = "none"
     }
     else{
