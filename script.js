ffa3f4f72a5ff834b21d268d9999614d86ce61c2
mantashasheikh <mantashasheikh95844@gmail.com>
2025-09-24 19:37:20 +0530

    java script code

--- script.js
+++ script.js
@@ -30,3 +30,14 @@ console.log(Boolean(0));
 console.log(Boolean("0"));      
 console.log(Boolean([]));       
 console.log(Boolean(undefined));
+
+
+
+ let str = "42";
+ let num = +str;
+ console.log(num);
+
+
+
+ let age = 17;
+ let msg = age >= 18 ? "Adult" : "Minor";
