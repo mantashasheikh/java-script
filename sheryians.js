849db2a9395c8109f1b30e28706b5094d71ef01e
mantashasheikh <mantashasheikh95844@gmail.com>
2025-10-18 08:42:29 +0530

    javascript become a developer

---
+++ sheryians.js
@@ -0,0 +1,6 @@
+let H = document.getElementById("heading");
+H.innerText = "you are moti";
+let para= document.getElementsByClassName("p");
+Para.innerText = "you are patli";
+let h = document.querySelectorAll(".head");
+h.innerText = "this is sweety . ";
