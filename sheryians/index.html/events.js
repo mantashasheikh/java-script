751478af7e571fca1230740aac27493a336d47df
mantashasheikh <mantashasheikh95844@gmail.com>
2025-10-28 10:21:37 +0530

    common events

--- sheryians/index.html/events.js
+++ sheryians/index.html/events.js
@@ -1,3 +1,8 @@
 document.getElementById("btn").addEventListener("click", () => {
   alert("Button clicked!");
 });
+
+
+document.getElementById("box").addEventListener("dblclick", () => {
+  alert("Box double-clicked!");
+});
