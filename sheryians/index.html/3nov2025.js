0e51d5754ff5f30cd893575a5079e609625630c9
mantashasheikh <mantashasheikh95844@gmail.com>
2025-11-07 09:26:32 +0530

    randome question

--- sheryians/index.html/3nov2025.js
+++ sheryians/index.html/3nov2025.js
@@ -2,3 +2,8 @@ let h = document.getElementById("heading")
 h.innerText = "this is a heading . "
 h.style.color = "red"
 h.style.backgroundColor = "yellow"
+
+let p = document.getElementById("para")
+p.innerHTML = "<h1>this is heading 1</h1>"
+p.style.color = "blue"
+p.style.backgroundColor = "pink"
